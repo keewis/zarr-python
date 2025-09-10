c03141c60c6d0030c89742274b690c552dce11c5
Justus Magin <keewis@posteo.de>
2025-09-10 16:32:32 +0200

    stop passing `chunks` to the ndbuffer

--- src/zarr/core/array.py
+++ src/zarr/core/array.py
@@ -1349,16 +1349,11 @@ class AsyncArray(Generic[T_ArrayMetadata]):
                     f"shape of out argument doesn't match. Expected {indexer.shape}, got {out.shape}"
                 )
         else:
-            if getattr(prototype.nd_buffer, "chunked", False):
-                kwargs = {"chunks": self.metadata.chunk_grid.chunk_shape}
-            else:
-                kwargs = {}
             out_buffer = prototype.nd_buffer.create(
                 shape=indexer.shape,
                 dtype=out_dtype,
                 order=self.order,
                 fill_value=self.metadata.fill_value,
-                **kwargs,
             )
         if product(indexer.shape) > 0:
             # need to use the order from the metadata for v2
@@ -1541,12 +1536,7 @@ class AsyncArray(Generic[T_ArrayMetadata]):
         # We accept any ndarray like object from the user and convert it
         # to a NDBuffer (or subclass). From this point onwards, we only pass
         # Buffer and NDBuffer between components.
-        if getattr(prototype.nd_buffer, "chunked", False):
-            kwargs = {"chunks": self.metadata.chunk_grid.chunk_shape}
-        else:
-            kwargs = {}
-
-        value_buffer = prototype.nd_buffer.from_ndarray_like(value, **kwargs)
+        value_buffer = prototype.nd_buffer.from_ndarray_like(value)
 
         # need to use the order from the metadata for v2
         _config = self._config
